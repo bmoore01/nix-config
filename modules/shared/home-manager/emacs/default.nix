7a29c412d7289eae7c949b579e834f626fcb5052
bmoore01 <bmoo@proton.me>
2024-10-04 09:42:00 +0200

    Removed some unused elisp

--- modules/shared/home-manager/emacs/default.nix
+++ modules/shared/home-manager/emacs/default.nix
@@ -3,21 +3,17 @@
     enable = true;
     package = (
       pkgs.emacsWithPackagesFromUsePackage {
-        config = ./config.org;
-        defaultInitFile = true;
-        alwaysEnsure = true;
-        alwaysTangle = true;
-        package = pkgs.emacs29-pgtk;
-        #package =
-        #  if pkgs.stdenv.isDarwin
-        #  then pkgs.emacs
-        #  else pkgs.emacs29-pgtk;
+	config = ./config.org;
+	defaultInitFile = true;
+	alwaysEnsure = true;
+	alwaysTangle = true;
+	package = pkgs.emacs29-pgtk;
 
-        extraEmacsPackages = epkgs:
-          with epkgs; [
-            vterm
-            treesit-grammars.with-all-grammars
-          ];
+	extraEmacsPackages = epkgs:
+	  with epkgs; [
+	    vterm
+	    treesit-grammars.with-all-grammars
+	  ];
       }
     );
   };
